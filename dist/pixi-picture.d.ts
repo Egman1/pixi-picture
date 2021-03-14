47f6f2fb4aff148960db4c6039dfa0920afd49ee
Troshkov Andrey <Andrey.Troshkov@enterra-inc.com>
2021-03-14 16:25:22 +0700

    add custom MULTIPLY blend mode (temp)

--- dist/pixi-picture.d.ts
+++ dist/pixi-picture.d.ts
@@ -1,4 +1,5 @@
-declare namespace PIXI.picture {
+/// <reference types="pixi.js" />
+declare namespace pixi_picture {
     class BackdropFilter extends PIXI.Filter {
         backdropUniformName: string;
         _backdropActive: boolean;
@@ -26,9 +27,9 @@ declare namespace PIXI {
         }
     }
 }
-declare namespace PIXI.picture {
+declare namespace pixi_picture {
 }
-declare namespace PIXI.picture {
+declare namespace pixi_picture {
     enum MASK_CHANNEL {
         RED = 0,
         GREEN = 1,
@@ -49,13 +50,17 @@ declare namespace PIXI.picture {
         apply(filterManager: PIXI.systems.FilterSystem, input: PIXI.RenderTexture, output: PIXI.RenderTexture, clearMode: PIXI.CLEAR_MODES): void;
     }
 }
-declare namespace PIXI.picture {
+declare namespace pixi_picture {
     namespace blends {
-        const NPM_BLEND = "if (b_src.a == 0.0) {\n    gl_FragColor = vec4(0, 0, 0, 0);\n    return;\n}\nvec3 Cb = b_src.rgb / b_src.a, Cs;\nif (b_dest.a > 0.0) {\n    Cs = b_dest.rgb / b_dest.a;\n}\n%NPM_BLEND%\nb_res.a = b_src.a + b_dest.a * (1.0-b_src.a);\nb_res.rgb = (1.0 - b_src.a) * Cs + b_src.a * B;\nb_res.rgb *= b_res.a;\n";
+        enum CUSTOM_BLEND_MODES {
+            MULTIPLY = 5
+        }
+        const NPM_BLEND = "if (b_src.a == 0.0) {\n            gl_FragColor = vec4(0, 0, 0, 0);\n            return;\n        }\n        vec3 Cb = b_src.rgb / b_src.a, Cs;\n        if (b_dest.a > 0.0) {\n            Cs = b_dest.rgb / b_dest.a;\n        }\n        \n        float outOp = b_src.a + b_dest.a * (1.0 - b_src.a);\n        vec3 B = b_src.rgb;\n        float cCA, eCA, result, color;\n        if (b_dest.a > 0.0) {\n            %NPM_BLEND%\n        }\n        b_res.rgb = B;\n        b_res.a = outOp;\n        ";
+        const MULTIPLY_PART = "cCA = b_dest.r;\n            eCA = b_src.r;\n            result = cCA * eCA;\n            color = (1.0 - b_src.a / outOp) * cCA + b_src.a / outOp * ((1.0 - b_dest.a) * eCA + b_dest.a * result);\n            B.r = color;\n            \n            cCA = b_dest.g;\n            eCA = b_src.g;\n            result = cCA * eCA;\n            color = (1.0 - b_src.a / outOp) * cCA + b_src.a / outOp * ((1.0 - b_dest.a) * eCA + b_dest.a * result);\n            B.g = color;\n            \n            cCA = b_dest.b;\n            eCA = b_src.b;\n            result = cCA * eCA;\n            color = (1.0 - b_src.a / outOp) * cCA + b_src.a / outOp * ((1.0 - b_dest.a) * eCA + b_dest.a * result);\n            B.b = color;\n        ";
         const OVERLAY_PART = "vec3 multiply = Cb * Cs * 2.0;\nvec3 Cb2 = Cb * 2.0 - 1.0;\nvec3 screen = Cb2 + Cs - Cb2 * Cs;\nvec3 B;\nif (Cs.r <= 0.5) {\n    B.r = multiply.r;\n} else {\n    B.r = screen.r;\n}\nif (Cs.g <= 0.5) {\n    B.g = multiply.g;\n} else {\n    B.g = screen.g;\n}\nif (Cs.b <= 0.5) {\n    B.b = multiply.b;\n} else {\n    B.b = screen.b;\n}\n";
         const HARDLIGHT_PART = "vec3 multiply = Cb * Cs * 2.0;\nvec3 Cs2 = Cs * 2.0 - 1.0;\nvec3 screen = Cb + Cs2 - Cb * Cs2;\nvec3 B;\nif (Cb.r <= 0.5) {\n    B.r = multiply.r;\n} else {\n    B.r = screen.r;\n}\nif (Cb.g <= 0.5) {\n    B.g = multiply.g;\n} else {\n    B.g = screen.g;\n}\nif (Cb.b <= 0.5) {\n    B.b = multiply.b;\n} else {\n    B.b = screen.b;\n}\n";
         const SOFTLIGHT_PART = "vec3 first = Cb - (1.0 - 2.0 * Cs) * Cb * (1.0 - Cb);\nvec3 B;\nvec3 D;\nif (Cs.r <= 0.5)\n{\n    B.r = first.r;\n}\nelse\n{\n    if (Cb.r <= 0.25)\n    {\n        D.r = ((16.0 * Cb.r - 12.0) * Cb.r + 4.0) * Cb.r;    \n    }\n    else\n    {\n        D.r = sqrt(Cb.r);\n    }\n    B.r = Cb.r + (2.0 * Cs.r - 1.0) * (D.r - Cb.r);\n}\nif (Cs.g <= 0.5)\n{\n    B.g = first.g;\n}\nelse\n{\n    if (Cb.g <= 0.25)\n    {\n        D.g = ((16.0 * Cb.g - 12.0) * Cb.g + 4.0) * Cb.g;    \n    }\n    else\n    {\n        D.g = sqrt(Cb.g);\n    }\n    B.g = Cb.g + (2.0 * Cs.g - 1.0) * (D.g - Cb.g);\n}\nif (Cs.b <= 0.5)\n{\n    B.b = first.b;\n}\nelse\n{\n    if (Cb.b <= 0.25)\n    {\n        D.b = ((16.0 * Cb.b - 12.0) * Cb.b + 4.0) * Cb.b;    \n    }\n    else\n    {\n        D.b = sqrt(Cb.b);\n    }\n    B.b = Cb.b + (2.0 * Cs.b - 1.0) * (D.b - Cb.b);\n}\n";
-        const MULTIPLY_FULL = "if (dest.a > 0.0) {\n   b_res.rgb = (dest.rgb / dest.a) * ((1.0 - src.a) + src.rgb);\n   b_res.a = min(src.a + dest.a - src.a * dest.a, 1.0);\n   b_res.rgb *= mult.a;\n}\n";
+        const MULTIPLY_FULL: string;
         const OVERLAY_FULL: string;
         const HARDLIGHT_FULL: string;
         const SOFTLIGHT_FULL: string;
@@ -64,15 +69,15 @@ declare namespace PIXI.picture {
     function getBlendFilter(blendMode: PIXI.BLEND_MODES): BlendFilter;
     function getBlendFilterArray(blendMode: PIXI.BLEND_MODES): BlendFilter[];
 }
-declare namespace PIXI.picture {
+declare namespace pixi_picture {
     class Sprite extends PIXI.Sprite {
         _render(renderer: PIXI.Renderer): void;
     }
 }
-declare namespace PIXI.picture {
+declare namespace pixi_picture {
     class TilingSprite extends PIXI.TilingSprite {
         _render(renderer: PIXI.Renderer): void;
     }
 }
-declare namespace PIXI.picture {
+declare namespace pixi_picture {
 }
--- dist/pixi-picture.js
+++ dist/pixi-picture.js
@@ -297,16 +297,21 @@ var pixi_picture;
 (function (pixi_picture) {
     var blends;
     (function (blends) {
-        blends.NPM_BLEND = "if (b_src.a == 0.0) {\n    gl_FragColor = vec4(0, 0, 0, 0);\n    return;\n}\nvec3 Cb = b_src.rgb / b_src.a, Cs;\nif (b_dest.a > 0.0) {\n    Cs = b_dest.rgb / b_dest.a;\n}\n%NPM_BLEND%\nb_res.a = b_src.a + b_dest.a * (1.0-b_src.a);\nb_res.rgb = (1.0 - b_src.a) * Cs + b_src.a * B;\nb_res.rgb *= b_res.a;\n";
+        var CUSTOM_BLEND_MODES;
+        (function (CUSTOM_BLEND_MODES) {
+            CUSTOM_BLEND_MODES[CUSTOM_BLEND_MODES["MULTIPLY"] = 5] = "MULTIPLY";
+        })(CUSTOM_BLEND_MODES = blends.CUSTOM_BLEND_MODES || (blends.CUSTOM_BLEND_MODES = {}));
+        blends.NPM_BLEND = "if (b_src.a == 0.0) {\n            gl_FragColor = vec4(0, 0, 0, 0);\n            return;\n        }\n        vec3 Cb = b_src.rgb / b_src.a, Cs;\n        if (b_dest.a > 0.0) {\n            Cs = b_dest.rgb / b_dest.a;\n        }\n        \n        float outOp = b_src.a + b_dest.a * (1.0 - b_src.a);\n        vec3 B = b_src.rgb;\n        float cCA, eCA, result, color;\n        if (b_dest.a > 0.0) {\n            %NPM_BLEND%\n        }\n        b_res.rgb = B;\n        b_res.a = outOp;\n        ";
+        blends.MULTIPLY_PART = "cCA = b_dest.r;\n            eCA = b_src.r;\n            result = cCA * eCA;\n            color = (1.0 - b_src.a / outOp) * cCA + b_src.a / outOp * ((1.0 - b_dest.a) * eCA + b_dest.a * result);\n            B.r = color;\n            \n            cCA = b_dest.g;\n            eCA = b_src.g;\n            result = cCA * eCA;\n            color = (1.0 - b_src.a / outOp) * cCA + b_src.a / outOp * ((1.0 - b_dest.a) * eCA + b_dest.a * result);\n            B.g = color;\n            \n            cCA = b_dest.b;\n            eCA = b_src.b;\n            result = cCA * eCA;\n            color = (1.0 - b_src.a / outOp) * cCA + b_src.a / outOp * ((1.0 - b_dest.a) * eCA + b_dest.a * result);\n            B.b = color;\n        ";
         blends.OVERLAY_PART = "vec3 multiply = Cb * Cs * 2.0;\nvec3 Cb2 = Cb * 2.0 - 1.0;\nvec3 screen = Cb2 + Cs - Cb2 * Cs;\nvec3 B;\nif (Cs.r <= 0.5) {\n    B.r = multiply.r;\n} else {\n    B.r = screen.r;\n}\nif (Cs.g <= 0.5) {\n    B.g = multiply.g;\n} else {\n    B.g = screen.g;\n}\nif (Cs.b <= 0.5) {\n    B.b = multiply.b;\n} else {\n    B.b = screen.b;\n}\n";
         blends.HARDLIGHT_PART = "vec3 multiply = Cb * Cs * 2.0;\nvec3 Cs2 = Cs * 2.0 - 1.0;\nvec3 screen = Cb + Cs2 - Cb * Cs2;\nvec3 B;\nif (Cb.r <= 0.5) {\n    B.r = multiply.r;\n} else {\n    B.r = screen.r;\n}\nif (Cb.g <= 0.5) {\n    B.g = multiply.g;\n} else {\n    B.g = screen.g;\n}\nif (Cb.b <= 0.5) {\n    B.b = multiply.b;\n} else {\n    B.b = screen.b;\n}\n";
         blends.SOFTLIGHT_PART = "vec3 first = Cb - (1.0 - 2.0 * Cs) * Cb * (1.0 - Cb);\nvec3 B;\nvec3 D;\nif (Cs.r <= 0.5)\n{\n    B.r = first.r;\n}\nelse\n{\n    if (Cb.r <= 0.25)\n    {\n        D.r = ((16.0 * Cb.r - 12.0) * Cb.r + 4.0) * Cb.r;    \n    }\n    else\n    {\n        D.r = sqrt(Cb.r);\n    }\n    B.r = Cb.r + (2.0 * Cs.r - 1.0) * (D.r - Cb.r);\n}\nif (Cs.g <= 0.5)\n{\n    B.g = first.g;\n}\nelse\n{\n    if (Cb.g <= 0.25)\n    {\n        D.g = ((16.0 * Cb.g - 12.0) * Cb.g + 4.0) * Cb.g;    \n    }\n    else\n    {\n        D.g = sqrt(Cb.g);\n    }\n    B.g = Cb.g + (2.0 * Cs.g - 1.0) * (D.g - Cb.g);\n}\nif (Cs.b <= 0.5)\n{\n    B.b = first.b;\n}\nelse\n{\n    if (Cb.b <= 0.25)\n    {\n        D.b = ((16.0 * Cb.b - 12.0) * Cb.b + 4.0) * Cb.b;    \n    }\n    else\n    {\n        D.b = sqrt(Cb.b);\n    }\n    B.b = Cb.b + (2.0 * Cs.b - 1.0) * (D.b - Cb.b);\n}\n";
-        blends.MULTIPLY_FULL = "if (dest.a > 0.0) {\n   b_res.rgb = (dest.rgb / dest.a) * ((1.0 - src.a) + src.rgb);\n   b_res.a = min(src.a + dest.a - src.a * dest.a, 1.0);\n   b_res.rgb *= mult.a;\n}\n";
+        blends.MULTIPLY_FULL = blends.NPM_BLEND.replace("%NPM_BLEND%", blends.MULTIPLY_PART);
         blends.OVERLAY_FULL = blends.NPM_BLEND.replace("%NPM_BLEND%", blends.OVERLAY_PART);
         blends.HARDLIGHT_FULL = blends.NPM_BLEND.replace("%NPM_BLEND%", blends.HARDLIGHT_PART);
         blends.SOFTLIGHT_FULL = blends.NPM_BLEND.replace("%NPM_BLEND%", blends.SOFTLIGHT_PART);
         blends.blendFullArray = [];
-        blends.blendFullArray[PIXI.BLEND_MODES.MULTIPLY] = blends.MULTIPLY_FULL;
+        blends.blendFullArray[CUSTOM_BLEND_MODES.MULTIPLY] = blends.MULTIPLY_FULL;
         blends.blendFullArray[PIXI.BLEND_MODES.OVERLAY] = blends.OVERLAY_FULL;
         blends.blendFullArray[PIXI.BLEND_MODES.HARD_LIGHT] = blends.HARDLIGHT_FULL;
         blends.blendFullArray[PIXI.BLEND_MODES.SOFT_LIGHT] = blends.SOFTLIGHT_FULL;
